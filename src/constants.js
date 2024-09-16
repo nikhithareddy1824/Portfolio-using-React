69ac764289a0e267e615dc99744f6b590b6d0711
nikhithareddy1824 <117994778+nikhithareddy1824@users.noreply.github.com>
2024-09-16 23:21:01 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Photographer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Nikhitha';
+
+export const experiences = [
+    {
+        'company': 'Psamathe Technologies, Bengaluru',
+        'role': 'Software Engineering Intern',
+        'duration': 'June 2024 - Present',
+        'logo': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwellfound.com%2Fcompany%2Fpsamathe-technologies&psig=AOvVaw0Klyiwn7PxCeT3RCRxPFap&ust=1725643069838000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNDxi8WnrIgDFQAAAAAdAAAAABAE',
+        'points': [
+            'Collaborated on the Ecommerce Project',
+            'Build the user and order micro services',
+            ' Integrated Rest API’s with AWS RDS (PostgreSQL)',
+             'Build frontend UI components in React.js ⚙️.'
+        ],
+        
+    },
+    {
+        'company': 'Take it easy Engineering | Company Assigned: Sain Informatix Private Limited',
+        'role': 'Full stack development online Intern ',
+        'duration': 'Nov 2022 - Jan 2023',
+        'logo': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fsainsolutionsbangalore%2F&psig=AOvVaw01c8l6P1xepXO3YUAmfKix&ust=1725643309611000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMipt7eorIgDFQAAAAAdAAAAABAE',
+        'points': [
+            'Gained experience with Technologies used:',
+             '• Front-End: Html, Css, JavaScript',
+             '• Database: Php, MySQ.',
+            'Worked on the cryptocurrency application which displays the live prices of various coins ,also helps in making the application  more user-friendly and improving its overall functionality.'
+        ],
+        
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
